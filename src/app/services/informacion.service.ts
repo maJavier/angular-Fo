53470f46d7faf9822983b2e6e333fac5b62fd842
Javier A <javieraguirre1697@gmail.com>
2017-09-01 01:49:44 -0300

    firebase 1

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,16 +6,33 @@ export class InformacionService {
 
   info: any = {};
   cargada: boolean = false;
+  cargada_sobre_nosotros: boolean = false;
+  equipo:any[] = [];
+  url = "https://portafolio-abb4a.firebaseio.com/equipo.json";
+
 
   constructor(public http: Http) {
 
+    this.carga_info();
+    this.cargaSobreNosotros();
+
+   }
+
+   public carga_info(){
     this.http.get("assets/data/info.pagina.json")
         .subscribe ( data => {
-          console.log(data.json());
-          this.cargada = true;
-          this.info = data.json();
+            this.cargada = true;
+            this.info = data.json();
         });
+   }
 
+   public cargaSobreNosotros(){
+    this.http.get(this.url)
+            .subscribe ( data => {
+              console.log(data.json());
+              this.cargada_sobre_nosotros = true;
+              this.equipo = data.json();
+            });
    }
 
 }
