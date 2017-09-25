5bcc512ed0c3ee144f261ecdcbd96e4722caca33
Javie Aguirre <javieraguirre1697@gmail.com>
2017-09-25 19:25:03 -0300

    'Angular'

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -8,6 +8,7 @@ export class ProductosService {
 
   productos: any[] = [];
   cargando: boolean = true;
+  productos_filtrado:any[] = [];
 
   constructor( private http: Http ) {
     
@@ -15,21 +16,54 @@ export class ProductosService {
 
    }
 
+
+    public buscar_producto(termino:string){
+
+
+     if( this.productos.length === 0 ){
+      this.cargar_productos().then( ()=> {
+        //terminó la carga!!! 
+        this.filtrar_producto(termino);
+      });
+     }else{
+      this.filtrar_producto(termino);
+    }
+   }
+
+    private filtrar_producto(termino:string){
+
+       this.productos_filtrado = [];
+       termino = termino.toLowerCase();
+
+       this.productos.forEach( prod =>{
+
+        if(prod.categoria.indexOf(termino)>=0 || prod.titulo.toLowerCase().indexOf(termino)>=0 ){
+          this.productos_filtrado.push(prod);
+        }
+
+        //console.log( prod );
+       });
+
+   }
+
    public cargar_productos(){
 
     this.cargando = true;
 
-    if (this.productos.length === 0){
-      this.http.get('https://portafolio-abb4a.firebaseio.com/productos_idx.json')
-          .subscribe( res => {
-          
-            // setTimeout( ()=>{
-              this.cargando = false;
-              this.productos = res.json();
-            // },1000)
+    let promesa = new Promise( ( resolve,reject )=> {
+        this.http.get('https://portafolio-abb4a.firebaseio.com/productos_idx.json')
+            .subscribe( res => {
             
-          });
-    }
+              // setTimeout( ()=>{
+                this.cargando = false;
+                this.productos = res.json();
+                resolve();
+              // },1000)
+              
+            });
+
+    });
+    return promesa;
    }
 
 
